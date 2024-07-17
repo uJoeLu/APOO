7445004c9bccd5c5157ea5ce2644a48ba8bc9bb5
Joel <jns18@discente.ifpe.edu.br>
2024-07-16 21:14:14 -0300

    add

--- src/apresentacao/App.java
+++ src/apresentacao/App.java
@@ -10,11 +10,20 @@ public class App {
              CadCliente<Cliente> cadCliente = CadCliente.getInstancia();
              cadCliente.cadastrarCliente(cliente);
 
-             Cliente cliente2 = new ClienteBuilder()
+             Cliente cliente1 = new ClienteBuilder()
              .setNome("Marcos Gomes")
              .setCpf("123456789")
              .setIdade(29)
              .setEmail("marcosgomes@gmail.com")
+             .build();
+              CadCliente<Cliente> cadCliente1 = CadCliente.getInstancia();
+              cadCliente1.cadastrarCliente(cliente1);
+              
+              Cliente cliente2 = new ClienteBuilder()
+             .setNome("Jadeilson Martin")
+             .setCpf("987654321")
+             .setIdade(22)
+             .setEmail("jadinho@gmail.com")
              .build();
               CadCliente<Cliente> cadClientes = CadCliente.getInstancia();
               cadClientes.cadastrarCliente(cliente2);
